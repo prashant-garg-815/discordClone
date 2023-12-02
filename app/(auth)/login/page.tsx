2f2a32724b744e3af8021fd6fb0f2817cc2e5a88
Prashant <prashantgarg815@gmail.com>
2023-12-02 21:18:38 +0530

    folder restructuring

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+const login = () => {
+    return (
+        <div>
+            This is the login page
+        </div>
+    )
+}
+
+export default login;
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+const register = () => {
+    return (
+        <div>
+            This the register page
+        </div>
+    )
+}
+
+export default register;
